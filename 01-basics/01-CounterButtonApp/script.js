53d535e1b80e87061ad45c6b28637040f7f1cc32
Aleksey Sheviakin <SheviakinAA@ileasing.ru>
2022-06-22 17:36:03 +0300

    Выполнил все задачи из Основ Vue

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,11 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+}).mount('#app')
--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
+
 
 // Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      changeId:null,
+      title:''
+    }
+  },
+  watch:{
+    changeId(newId){
+      this.fetchMeetupById(newId)
+      .then(data=>this.title=data.title)
+    }
+  },
+  methods:{
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+    }
+  }
+}).mount('#app')
